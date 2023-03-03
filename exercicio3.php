1bed423467d560d53f1af1cb263307ab5bef4cb9
Franklin Silva <65989304+frankstylle@users.noreply.github.com>
2023-03-03 18:38:57 -0300

    Create exercicio3.php

---
+++ exercicio3.php
@@ -0,0 +1,29 @@
+3) Descubra a lógica e complete o próximo elemento:
+a) 1, 3, 5, 7, ___
+// Definindo os três primeiros números da sequência
+$num1 = 1;
+$num2 = 3;
+$num3 = 5;
+
+// Calculando o próximo número da sequência
+$prox_num = $num3 + 2;
+
+// Imprimindo o próximo número da sequência
+echo $prox_num;
+
+b) 2, 4, 8, 16, 32, 64, ____
+// Definindo os sete primeiros números da sequência
+$num1 = 2;
+$num2 = 4;
+$num3 = 8;
+$num4 = 16;
+$num5 = 32;
+$num6 = 64;
+
+// Calculando o próximo número da sequência
+$prox_num = $num6 * 2;
+
+// Imprimindo o próximo número da sequência
+echo $prox_num;
+
+c) 0, 1, 4, 9, 16, 25, 36, ____
